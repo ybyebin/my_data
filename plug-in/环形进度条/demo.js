f3398d017102b29d85ccacfc975af591c72afa5f
ybyebin <yebin@bayax.cn>
2018-10-19 11:35:22 +0800

    .

--- plug-in/环形进度条/demo.js
+++ plug-in/环形进度条/demo.js
@@ -28,4 +28,31 @@ setTimeout(function(){
     radials.animate(90)
 },1000)
 
+
+
+
+
+
+
+
+
+var radials = radiallndicator('#indicatorContainer2',{
+    barColor: '#87CEEB',
+    barBgColor:'#e1e1e1',
+    barWidth: 30,
+    initValue: 0,
+    radius:200,
+    roundCorner: true
+    // fontSize:30
+}); 
+    $('.btn-exchange').on('click', function () {
+        console.log('点击按钮=====')
+        console.log('查看:'+devicePixelRatio)
+        console.log('=====================')
+        radials.animate(10)
+
+           
+    })
+
+
 });;
